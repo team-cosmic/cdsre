442c695f7b0d73b0d4336e60153d187ee9223cff
Arpaesis <arpaesis@gmail.com>
2019-04-10 16:43:26 -0400

    Began implementing pokemon editor view.

---
+++ src/main/kotlin/cdsre/PokemonViewController.kt
@@ -0,0 +1,14 @@
+package cdsre
+
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.layout.AnchorPane
+import java.net.URL
+import java.util.*
+
+class PokemonViewController: Initializable {
+
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+    }
+
+}
---
+++ src/main/kotlin/cdsre/controllers/details/PokemonDetailController.kt
@@ -0,0 +1,14 @@
+package cdsre.controllers.details
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+
+class PokemonDetailController: Initializable {
+
+
+    override fun initialize(p0: URL?, p1: ResourceBundle?) {
+        //TODO:
+    }
+}
