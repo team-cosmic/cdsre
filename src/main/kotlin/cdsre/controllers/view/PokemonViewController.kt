5dcfdc0ec28b4f56477f539120e1199e6db1ea51
Arpaesis <arpaesis@gmail.com>
2019-04-11 18:19:52 -0400

    Added backend code for pokemon editor spinners.

--- src/main/kotlin/cdsre/controllers/view/PokemonViewController.kt
+++ src/main/kotlin/cdsre/controllers/view/PokemonViewController.kt
@@ -3,9 +3,69 @@ package cdsre.controllers.view
 import javafx.fxml.Initializable
 import java.net.URL
 import java.util.*
+import javafx.fxml.FXML
+import javafx.scene.control.*
+import javafx.scene.layout.AnchorPane
+
+
 
 class PokemonViewController: Initializable {
 
+	@FXML
+	lateinit var pokemon_view: AnchorPane
+
+	@FXML
+	lateinit var selectalltms: Button
+
+	@FXML
+	lateinit var deselectalltms: Button
+
+	@FXML
+	lateinit var learnlevel: Spinner<*>
+
+	@FXML
+	lateinit var move: ChoiceBox<*>
+
+	@FXML
+	lateinit var addmove: Button
+
+	@FXML
+	lateinit var deletemove: Button
+
+	@FXML
+	lateinit var selectallhms: Button
+
+	@FXML
+	lateinit var deselectallhms: Button
+
+	@FXML
+	lateinit var evolutionmethod: ChoiceBox<*>
+
+	@FXML
+	lateinit var evolutionnumber: Slider
+
+	@FXML
+	lateinit var evolutionlevel: Spinner<*>
+
+	@FXML
+	lateinit var happinessrequired: Spinner<*>
+
+	@FXML
+	lateinit var evolutionitem: ChoiceBox<*>
+
+	@FXML
+	lateinit var evolutionpokemon: ChoiceBox<*>
+
+	@FXML
+	lateinit var evolutionattack: ChoiceBox<*>
+
+	@FXML
+	lateinit var evolvesinto: ChoiceBox<*>
+
 	override fun initialize(p0: URL?, p1: ResourceBundle?) {
+		learnlevel.valueFactory = SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE)
+		evolutionlevel.valueFactory = SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE)
+		happinessrequired.valueFactory = SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE)
 	}
+
 }
