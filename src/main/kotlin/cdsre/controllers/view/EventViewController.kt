4185d89f838e30285a830abd4dfd68fbec1c7518
Arpaesis <arpaesis@gmail.com>
2019-04-10 21:39:50 -0400

    Refactored view controller locations.

--- src/main/kotlin/cdsre/controllers/view/EventViewController.kt
+++ src/main/kotlin/cdsre/controllers/view/EventViewController.kt
@@ -1,4 +1,4 @@
-package cdsre
+package cdsre.controllers.view
 
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
@@ -6,11 +6,11 @@ import javafx.scene.layout.AnchorPane
 import java.net.URL
 import java.util.*
 
-class MatrixViewController: Initializable {
+class EventViewController: Initializable {
 
 	override fun initialize(p0: URL?, p1: ResourceBundle?) {
 	}
 
 	@FXML
-	lateinit var matrix_view: AnchorPane
+	lateinit var event_view: AnchorPane
 }
--- src/main/kotlin/cdsre/controllers/view/MapHeaderViewController.kt
+++ src/main/kotlin/cdsre/controllers/view/MapHeaderViewController.kt
@@ -1,4 +1,4 @@
-package cdsre
+package cdsre.controllers.view
 
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
