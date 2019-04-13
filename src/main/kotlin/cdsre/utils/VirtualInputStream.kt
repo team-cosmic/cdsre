7a697245959bec78190224cf7535c278a5a16f1a
CraftSpider <runetynan@gmail.com>
2019-04-13 15:34:53 -0400

    Simple virtual files now support input and output streams

---
+++ src/main/kotlin/cdsre/utils/VirtualInputStream.kt
@@ -0,0 +1,28 @@
+package cdsre.utils
+
+import java.io.File
+import java.io.InputStream
+
+class VirtualInputStream(file: File, offset: Long, val length: Long) : InputStream() {
+
+    private val realInputStream: InputStream = file.inputStream()
+    private var curOffset: Long = 0
+
+    init {
+        realInputStream.skip(offset)
+    }
+
+    override fun read(): Int {
+        if (curOffset >= length) {
+            return -1
+        }
+        curOffset += 1
+        return realInputStream.read()
+    }
+
+    override fun close() {
+        super.close()
+        realInputStream.close()
+    }
+
+}
