7b376fb9aefc1d8f249d566c80bed61238ffab23
CraftSpider <runetynan@gmail.com>
2019-04-13 16:38:19 -0400

    Add signed endian conversions

--- src/main/kotlin/cdsre/utils/Endianness.kt
+++ src/main/kotlin/cdsre/utils/Endianness.kt
@@ -4,27 +4,52 @@ class Endianness {
     enum class Endian {
         LITTLE, BIG;
     }
-    
+
+    /**
+     * Note: Unless otherwise noted, methods in this object do
+     * not check if inputs are actually bytes
+     */
     companion object {
         /**
-         * Note: Does not check if the inputs are actually bytes
+         * @param b1 first byte
+         * @param b2 seconds byte
+         * @param endianness which endian to pack bytes with
+         * @return a Short packed with the bytes
+         */
+        fun toShort(b1: Int, b2: Int, endianness: Endian): Short {
+            return toShort(intArrayOf(b1, b2), endianness)
+        }
+
+        /**
+         * @param bytes an array containing the bytes
+         * @param endianness which endian to pack bytes with
+         * @return a Short packed with the bytes
+         */
+        fun toShort(bytes: IntArray, endianness: Endian): Short {
+            var short: Int = 0
+            if(endianness == Endian.LITTLE)
+                for(i in 1 downTo 0) short += (bytes[i] shl (8*i))
+            else
+                for(i in 0..1) short += (bytes[i] shl (8*(1-i)))
+            return short.toShort()
+        }
+
+        /**
          * @param b1 first byte
          * @param b2 second byte
          * @param endianness which endian to pack bytes with
          * @return a UShort packed with the bytes
          */
-        public fun toUShort(b1: Int, b2: Int, endianness: Endian): UShort{
+        fun toUShort(b1: Int, b2: Int, endianness: Endian): UShort {
             return toUShort(intArrayOf(b1, b2), endianness)
         }
         
-        
         /**
-         * Note: Does not check if the inputs are actually bytes
          * @param bytes an array containing the bytes
          * @param endianness which endian to pack bytes with
          * @return a UShort packed with the bytes
          */
-        public fun toUShort(bytes: IntArray, endianness: Endian): UShort{
+        fun toUShort(bytes: IntArray, endianness: Endian): UShort {
             var short: Int = 0
             if(endianness == Endian.LITTLE)
                 for(i in 1 downTo 0) short += (bytes[i] shl (8*i))
@@ -32,9 +57,34 @@ class Endianness {
                 for(i in 0..1) short += (bytes[i] shl (8*(1-i)))
             return short.toUShort()
         }
+
+        /**
+         * @param b1 first byte
+         * @param b2 second byte
+         * @param b3 third byte
+         * @param b4 fourth byte
+         * @param endianness which endian to pack bytes with
+         * @return an Int packed with the bytes
+         */
+        fun toInt(b1: Int, b2: Int, b3: Int, b4: Int, endianness: Endian): Int {
+            return toInt(intArrayOf(b1, b2, b3, b4), endianness)
+        }
+
+        /**
+         * @param bytes an array containing the bytes
+         * @param endianness which endian to pack bytes with
+         * @return an Int packed with the bytes
+         */
+        fun toInt(bytes: IntArray, endianness: Endian): Int {
+            var int: Int = 0
+            if(endianness == Endian.LITTLE)
+                for(i in 3 downTo 0) int += (bytes[i] shl (8*i))
+            else
+                for(i in 0..3) int += (bytes[i] shl (8*(3-i)))
+            return int
+        }
         
         /**
-         * Note: Does not check if the inputs are actually bytes
          * @param b1 first byte
          * @param b2 second byte
          * @param b3 third byte
@@ -42,17 +92,16 @@ class Endianness {
          * @param endianness which endian to pack bytes with
          * @return a UInt packed with the bytes
          */
-        public fun toUInt(b1: Int, b2: Int, b3: Int, b4: Int, endianness: Endian): UInt{
+        fun toUInt(b1: Int, b2: Int, b3: Int, b4: Int, endianness: Endian): UInt {
             return toUInt(intArrayOf(b1, b2, b3, b4), endianness)
         }
         
         /**
-         * Note: Does not check if the inputs are actually bytes
          * @param bytes an array containing the bytes
          * @param endianness which endian to pack bytes with
          * @return a UInt packed with the bytes
          */
-        public fun toUInt(bytes: IntArray, endianness: Endian): UInt{
+        fun toUInt(bytes: IntArray, endianness: Endian): UInt {
             var int: Int = 0
             if(endianness == Endian.LITTLE)
                 for(i in 3 downTo 0) int += (bytes[i] shl (8*i))
@@ -60,9 +109,38 @@ class Endianness {
                 for(i in 0..3) int += (bytes[i] shl (8*(3-i)))
             return int.toUInt()
         }
+
+        /**
+         * @param b1 first byte
+         * @param b2 second byte
+         * @param b3 third byte
+         * @param b4 fourth byte
+         * @param b5 fifth byte
+         * @param b6 sixth byte
+         * @param b7 seventh byte
+         * @param b8 eigth byte
+         * @param endianness which endian to pack bytes with
+         * @return a Long packed with the bytes
+         */
+        fun toLong(b1: Int, b2: Int, b3: Int, b4: Int, b5: Int, b6: Int, b7: Int, b8: Int, endianness: Endian): Long {
+            return toLong(intArrayOf(b1, b2, b3, b4, b5, b6, b7, b8), endianness)
+        }
+
+        /**
+         * @param bytes an array containing the bytes
+         * @param endianness which endian to pack bytes with
+         * @return a Long packed with the bytes
+         */
+        fun toLong(bytes: IntArray, endianness: Endian): Long {
+            var long: Long = 0
+            if(endianness == Endian.LITTLE)
+                for(i in 7 downTo 0) long += (bytes[i].toLong() shl (8*i))
+            else
+                for(i in 0..7) long += (bytes[i].toLong() shl (8*(7-i)))
+            return long
+        }
         
         /**
-         * Note: Does not check if the inputs are actually bytes
          * @param b1 first byte
          * @param b2 second byte
          * @param b3 third byte
@@ -74,17 +152,16 @@ class Endianness {
          * @param endianness which endian to pack bytes with
          * @return a ULong packed with the bytes
          */
-        public fun toULong(b1: Int, b2: Int, b3: Int, b4: Int, b5: Int, b6: Int, b7: Int, b8: Int, endianness: Endian): ULong{
+        fun toULong(b1: Int, b2: Int, b3: Int, b4: Int, b5: Int, b6: Int, b7: Int, b8: Int, endianness: Endian): ULong {
             return toULong(intArrayOf(b1, b2, b3, b4, b5, b6, b7, b8), endianness)
         }
         
         /**
-         * Note: Does not check if the inputs are actually bytes
          * @param bytes an array containing the bytes
          * @param endianness which endian to pack bytes with
          * @return a ULong packed with the bytes
          */
-        public fun toULong(bytes: IntArray, endianness: Endian): ULong{
+        fun toULong(bytes: IntArray, endianness: Endian): ULong {
             var long: Long = 0
             if(endianness == Endian.LITTLE)
             	for(i in 7 downTo 0) long += (bytes[i].toLong() shl (8*i))
@@ -92,25 +169,53 @@ class Endianness {
                 for(i in 0..7) long += (bytes[i].toLong() shl (8*(7-i)))
             return long.toULong()
         }
+
+        /**
+         * @param short unsigned short to unpack
+         * @param endianness which endian to unpack bytes by
+         * @return an array containing the bytes ordered by endianness
+         */
+        fun fromShort(short: Short, endianness: Endian): IntArray {
+            val int = short.toInt()
+            return toIntArray(int, 2, endianness)
+        }
         
         /**
          * @param ushort unsigned short to unpack
          * @param endianness which endian to unpack bytes by
-         * @param an array containing the bytes ordered by endianness
+         * @return an array containing the bytes ordered by endianness
          */
-        public fun fromUShort(ushort: UShort, endianness: Endian): IntArray{
+        fun fromUShort(ushort: UShort, endianness: Endian): IntArray {
             val int = ushort.toInt()
             return toIntArray(int, 2, endianness)
+        }
+
+        /**
+         * @param int unsigned int to unpack
+         * @param endianness which endian to unpack bytes by
+         * @return an array containing the bytes ordered by endianness
+         */
+        fun fromInt(int: Int, endianness: Endian): IntArray {
+            return toIntArray(int, 4, endianness)
         }
         
         /**
          * @param uint unsigned int to unpack
          * @param endianness which endian to unpack bytes by
-         * @param an array containing the bytes ordered by endianness
+         * @return an array containing the bytes ordered by endianness
          */
-        public fun fromUInt(uint: UInt, endianness: Endian): IntArray{
+        fun fromUInt(uint: UInt, endianness: Endian): IntArray {
             val int = uint.toInt()
             return toIntArray(int, 4, endianness)
+        }
+
+        /**
+         * @param long unsigned long to unpack
+         * @param endianness which endian to unpack bytes by
+         * @return an array containing the bytes ordered by endianness
+         */
+        fun fromLong(long: Long, endianness: Endian): IntArray {
+            return longToIntArray(long, endianness)
         }
         
         /**
@@ -118,7 +223,7 @@ class Endianness {
          * @param endianness which endian to unpack bytes by
          * @return an array containing the bytes ordered by endianness
          */
-        public fun fromULong(ulong: ULong, endianness: Endian): IntArray {
+        fun fromULong(ulong: ULong, endianness: Endian): IntArray {
             val long = ulong.toLong()
             return longToIntArray(long, endianness)
         }
@@ -132,9 +237,9 @@ class Endianness {
          */
         private fun toIntArray(int: Int, bytes: Int, endianness: Endian): IntArray {
             if(endianness == Endian.LITTLE) {
-                return IntArray(bytes){ i -> ((int shr (i*8)) and 0xFF).toInt()}
+                return IntArray(bytes){ i -> ((int shr (i*8)) and 0xFF)}
             } else {
-                return IntArray(bytes){ i -> ((int shr ((bytes-1-i)*8)) and 0xFF).toInt()}
+                return IntArray(bytes){ i -> ((int shr ((bytes-1-i)*8)) and 0xFF)}
             }
         }
         
