52ad73c2672544419bc97cabeab604799ca244cf
Arpaesis <arpaesis@gmail.com>
2019-04-15 23:29:10 -0400

    Added more complexity to project creation.

--- src/main/kotlin/cdsre/controllers/project/ProjectSetup.kt
+++ src/main/kotlin/cdsre/controllers/project/ProjectSetup.kt
@@ -4,21 +4,39 @@ import javafx.scene.Scene
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.stage.Stage
+import javafx.fxml.FXML
+import javafx.scene.control.Button
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.AnchorPane
+
+
 
 
 class ProjectSetup {
+
+    @FXML
+    lateinit var projectsetup: AnchorPane
 
     fun start()
     {
         val root: Parent = FXMLLoader.load(javaClass.classLoader.getResource("graphics/project/projectsetup.fxml"))
 
-        val stage = Stage()
-
+        var stage = Stage()
         stage.title = "Project Setup"
-        stage.scene = Scene(root, 400.0, 600.0)
+        stage.scene = Scene(root)
 
         stage.scene.stylesheets.add(this.javaClass.classLoader.getResource("css/main.css").toExternalForm())
 
         stage.show()
+    }
+
+    @FXML
+    fun close(event: MouseEvent) {
+        projectsetup.scene.window.hide()
+    }
+
+    @FXML
+    fun createProject(event: MouseEvent) {
+        //TODO: Implement
     }
 }
