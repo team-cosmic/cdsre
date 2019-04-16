5c5a04a776f69493d0f2719ddcc1b01195cd7c59
Arpaesis <arpaesis@gmail.com>
2019-04-15 21:58:41 -0400

    Finished implementing other master controllers.

---
+++ src/main/kotlin/cdsre/controllers/master/TextMasterController.kt
@@ -0,0 +1,11 @@
+package cdsre.controllers.master
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+class TextMasterController: Initializable {
+
+	override fun initialize(p0: URL?, p1: ResourceBundle?) {
+	}
+}
