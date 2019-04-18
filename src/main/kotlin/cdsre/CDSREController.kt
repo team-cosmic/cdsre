3ed87d4b0d1ce7f209273159e20c4428266aa353
Arpaesis <arpaesis@gmail.com>
2019-04-17 22:26:13 -0400

    Closing the main window now closes the entire program.

--- src/main/kotlin/cdsre/CDSREController.kt
+++ src/main/kotlin/cdsre/CDSREController.kt
@@ -19,7 +19,6 @@ import java.net.URI
 import java.util.*
 import java.net.URL
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStreamReader
 import java.io.File
 import java.lang.Exception
